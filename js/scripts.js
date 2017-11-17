f0974c8bfad5b8c3d5230e8f77422295662fbd9c
naveensecexp <31397538+naveensecexp@users.noreply.github.com>
2017-11-17 10:24:20 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@
 
     // custom scrollbar
 
-    $("html").niceScroll({styler:"fb",cursorcolor:"rgb(16, 167, 175)", cursorwidth: '6', cursorborderradius: '10px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
+    $("html").niceScroll({styler:"fb",cursorcolor:"#fc4a1a", cursorwidth: '6', cursorborderradius: '10px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
 
-    $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"rgb(16, 167, 175)", cursorwidth: '6', cursorborderradius: '0',autohidemode: 'false', background: '#F1F1F1', spacebarenabled:false, cursorborder: '0'});
+    $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"#fc4a1a", cursorwidth: '6', cursorborderradius: '0',autohidemode: 'false', background: '#F1F1F1', spacebarenabled:false, cursorborder: '0'});
 
 	
 	
